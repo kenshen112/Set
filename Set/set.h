cacd8bb26f040a252c1df7a3449b50092f0e035c
t o <finalfantasytimothy@gmail.com>
2019-02-04 20:32:35 -0700

    Fixed prototypes and added some basic code

--- Set/set.h
+++ Set/set.h
@@ -13,17 +13,36 @@ private:
 public:
 	set()
 	{
-
+		data = nullptr;
+		numElements = 0;
+		numCapacity = 0;
 	}
 
 	set(int numCapacity)
 	{
-
+		data = nullptr;
+		numElements = 0;
+		this->numCapacity = numCapacity;
 	}
 
 	set(const set& rhs)
 	{
+		assert(rhs.numCapacity >= 0);
+		
+		if (numCapacity == rhs.size())
+		{
+			resize(rhs.size());
+		}
 
+		try
+		{
+			data = new T[rhs.numCapacity];
+		}
+		catch (std::bad_alloc)
+		{
+			throw "ERROR: Unable to allocate buffer";
+		}
+		numCapacity = rhs.numCapacity;
 	}
 
 	
@@ -32,16 +51,35 @@ public:
 	int resize(int numCapacity);
 	bool empty();
 	void clear();
-	void insert(T item);
-	set union (set &rhs);
-	set intersection(rhs &set);
+	void  insert(const T & t);
+	set operator || (const set <T> & rhs);
+	set operator && (const set <T> & rhs);
 	set operator=(set& rhs);
-	set difference(set &rhs);
+	set operator-(const set <T> & rhs);
 
 	class set <T> ::iterator
 	{
 
+	private:
+		
+	public:
+		iterator()
+		{
 
+		}
+
+		iterator(const iterator &rhs)
+		{
+
+		}
+
+
+		iterator operator=(iterator it);
+		bool isEquals(iterator it);
+		bool isNE1ual(iterator it);
+		iterator increment();
+		iterator decrement();
+		T dereference();
 	};
 
 	iterator find(T t);
@@ -53,11 +91,117 @@ public:
 	{
 
 	}
-
-	
-	
 };
 
+template<class T>
+int set<T>::size()
+{
+	return numElements - numCapacity + 1;
+}
+
+template<class T>
+int set<T>::findIndex(T item)
+{
+	int begining = 0;
+	int ending = numElements - 1;
+
+	while (begining <= ending)
+	{
+		int middle = (begining + ending) / 2;
+		if (item == data[middle])
+		{
+			return middle;
+		}
+
+		else if (item < data[middle]) {
+			ending = middle - 1;
+		}
+		else
+		{
+			begining = middle + 1;
+		}
+
+	}
+
+	return numElements;
+}
+
+template<class T>
+int set<T>::resize(int numCapacity)
+{
+	return 0;
+}
+
+template<class T>
+bool set<T>::empty()
+{
+	return false;
+}
+
+template<class T>
+void set<T>::clear()
+{
+}
+
+template<class T>
+void set<T>::insert(const T & t)
+{
+
+	int ifFound = find(item);
+	if (ifFound == 0)
+	{
+		data[insertIndex] = item;
+		numItems++;
+	}
 
 
 
+}
+
+template<class T>
+set<T> set<T>::operator||(const set<T>& rhs)
+{
+	return set();
+}
+
+template<class T>
+set<T> set<T>::operator && (const set <T> & rhs)
+{
+	return set();
+}
+
+template<class T>
+set<T> set<T>::operator=(set & rhs)
+{
+	return set();
+}
+
+template<class T>
+set<T> set<T>::operator-(const set <T> & rhs)
+{
+	return set();
+}
+
+template<class T>
+set<T>::iterator set<T>::find(T t)
+{
+	return iterator();
+}
+
+template<class T>
+set<T>::iterator set<T>::erase(iterator it)
+{
+	return iterator();
+}
+
+template<class T>
+set<T>::iterator set<T>::begin()
+{
+	return iterator();
+}
+
+template<class T>
+set<T>::iterator set<T>::end()
+{
+	return iterator();
+}
